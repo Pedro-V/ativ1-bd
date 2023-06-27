0c6977fe70c8cc60bf5bb21c7fcbab8cff3ae802
Pedro-V <mingas62@hotmail.com>
2023-06-27 02:12:46 -0300

    modelo User criado

--- src/user_app/models.py
+++ src/user_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    """
+    Um usuário cadastrado.
+    """
+    cpf = models.BigIntegerField(primary_key=True)
+    nome = models.CharField(max_length=80)
+    data_nascimento = models.DateField()
