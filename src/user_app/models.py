1ebdd3a8084f34b9a11711a2ce6c1a2aeba54a22
Pedro-V <mingas62@hotmail.com>
2023-07-13 00:44:37 -0300

    removido src/

--- src/user_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Usuario(models.Model):
-    """
-    Um usuário cadastrado.
-    """
-    cpf = models.BigIntegerField(primary_key=True)
-    nome = models.CharField(max_length=80)
-    data_nascimento = models.DateField()
