f43b1b9e6e0db9bb254fbf65c91e30a931639147
Pedro-V <mingas62@hotmail.com>
2023-08-14 18:42:50 -0300

    corrigido lista dupla de usuarios

--- user_app/urls.py
+++ user_app/urls.py
@@ -3,10 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from user_app import views
 
 urlpatterns = [
-    # either the empty path or users
     path('', views.UsuarioList.as_view()),
-    path('users/', views.UsuarioList.as_view()), 
-    path('users/<int:pk>/', views.UsuarioDetail.as_view()),
+    path('<int:pk>/', views.UsuarioDetail.as_view()),
 ]
 
 # permite tratamento apropriado de diferentes formatos
